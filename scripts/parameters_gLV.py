5155b6b8ec75d7a458b1c6c45270bab2700692af
Ammaar Saeed <ammsa23@gmail.com>
2022-04-01 16:41:37 -0400

    reorganizing

---
+++ scripts/parameters_gLV.py
@@ -0,0 +1,229 @@
+
+__author__ = "Ammaar A. Saeed"
+__email__ = "aasaeed@college.harvard.edu"
+
+'''
+Description: 
+------------
+This file contains functions that generate parameters for the generalized 
+Lotka-Volterra Model with evolution represented under a stochastic framework
+for S total species and a mutant of one of the species
+
+Parameter Generation: 
+---------------------
+The birth and death rate parameters are drawn from a folded standard Normal 
+distribution 
+The interaction matrix parameters are drawn from a standard Normal distribution
+The perturbations on the mutant are drawn from a Uniform distribution on 
+the interval [-0.1,0.1]
+'''
+
+import numpy as np
+from numpy.random import default_rng
+
+def generate_birth_death_rates(
+        S: int, 
+        seed: int
+    ): 
+    '''
+    Generate a pair of positive birth and death rates for each species
+    
+    Parameters: 
+    -----------
+    int S - total number of species; the number of birth and death rates to 
+    generate
+    int seed - random seed integer 
+
+    Returns: 
+    --------
+    np.array - array of birth (column 1) and death (column 2) rates for each 
+    species (shape: (S, 2))
+    '''
+
+    # define a rng object
+    rng = default_rng(seed)
+
+    return np.abs(rng.normal(loc = 0, scale = 1, size = (S, 2)))
+
+def mutant_birth_death_rate(
+        birth_death: np.array, 
+        seed: int
+    ): 
+    '''
+    Perturb the given birth and death rates from draws of a Uniform distribution 
+    on the interval [-0.1, 0.1]
+
+    Parameters: 
+    -----------
+    np.array birth_death - birth and death rates of the species of interest; 
+    birth and death rates to be perturbed 
+    int seed - random seed integer
+
+    Returns: 
+    --------
+    np.array - array of perturbed birth and death rates for the mutant 
+    species of interest (shape: (1, 2))
+    '''
+
+    # define a rng object
+    rng = default_rng(seed)
+
+    return birth_death + rng.uniform(low = -0.1, high = 0.1, size = (1,2))
+
+def pre_interaction_matrix(
+        S: int, 
+        seed: int, 
+        method = "May", 
+        rho = None, 
+        tol = None
+    ): 
+    '''
+    Generate an interaction matrix for the relationships between species 
+    using either the method described by May or Alessina-Tang
+
+    Parameters: 
+    -----------
+    int S - total number of species; will generate a total of S(S-1) interaction
+    parameters 
+    int seed - random seed integer
+    string method - the method to generate the interaction matrix: the two
+    options are either (1) "May" or (2) "Alessina-Tang"
+    float rho - correlation coefficient between bivariate Normal random 
+    variables 
+    float tol - tolerance level for maintaining connections in the interaction
+    matrix
+
+    Returns: 
+    --------
+    np.array - interaction matrix describing the relationship between the S
+    species (shape: (S, S), diagonal elements are set to 0)
+    '''
+
+    # define a rng object
+    rng = default_rng(seed)
+
+    interaction_matrix = np.zeros((S, S))
+
+    # using the May method
+    if (method == "May"): 
+
+        # generate the interaction matrix with all entries filled 
+        interaction_matrix = rng.normal(loc = 0, scale = 1, size = (S, S))
+
+    elif (method == "Alessina-Tang"): 
+
+        if (rho == None or tol == None): 
+            raise ValueError
+
+        # generate paired data from a bivariate normal distribution 
+        pairs = rng.multivariate_normal(np.array([0, 0]), 
+                                           np.array([1, rho, rho, 1]).reshape(2, 2))
+
+        # place the paired data into the matrix 
+        interaction_matrix = np.zeros((S, S))
+        interaction_matrix[np.triu_indices(S)] = pairs[:,0]
+        interaction_matrix[np.tril_indices(S)] = pairs[:,1]
+
+        # use the provided tolerance value to determine which relationships
+        # to maintain
+        paired = rng.uniform(S ** 2, size = (S, S)) <= tol 
+        paired[np.tril_indices(S)] = 0
+        np.fill_diagonal(paired, 0)
+
+        # set the interaction matrix according to the rngly 
+        # generated pairs 
+        interaction_matrix = interaction_matrix * paired
+
+    # set the diagonal elements to 0
+    np.fill_diagonal(interaction_matrix, 0)
+
+    return interaction_matrix
+
+def mutant_interaction_matrix(
+        interaction_row: np.array, 
+        interaction_col: np.array, 
+        seed: int
+    ): 
+    '''
+    Perturb the given interaction matrix row and column from draws 
+    of a Uniform distribution on the interval [-0.1, 0.1]
+
+    Parameters: 
+    -----------
+    np.array interaction_row - row of the interaction matrix for the 
+    species of interest; row of the interaction matrix to be perturbed
+    np.array interaction_col - column of the interaction matrix for
+    the species of interest; column of the interaction matrix to be 
+    perturbed
+    int seed - random seed integer
+
+    Returns: 
+    --------
+    np.array (1) - array of perturbed interaction column information 
+    (shape: (n-1,))
+    np.array (2) - array of perturbed interaction row information 
+    (shape: (n,))
+    '''
+
+    # define a rng object
+    rng = default_rng(seed)
+
+    # perturb the interaction column first, dropping the last term (0)
+    interaction_col = interaction_col + \
+                      rng.uniform(low = -0.1, high = 0.1, 
+                      size = (interaction_col.shape[0]))
+
+    # perturb the interaction row
+    interaction_row = interaction_row + \
+                      rng.uniform(low = -0.1, high = 0.1, 
+                      size = (interaction_row.shape[0]))
+    interaction_row = np.hstack([interaction_row, [0]])
+
+    return interaction_row, interaction_col
+
+def generate_interaction_matrix(
+        S: int, 
+        seed: int, 
+        method = "May"
+    ): 
+    '''
+    Generate an interaction matrix for the relationships between all
+    species and the mutant of the species of interest using either 
+    the method described by May or Alessina-Tang
+
+    Parameters: 
+    -----------
+    int S - total number of species; will generate a total of S(S+1) interaction
+    parameters
+    int seed - random seed integer
+    string method - the method to generate the interaction matrix: the two
+    options are either (1) "May" or (2) "Alessina-Tang"
+
+    Returns: 
+    --------
+    np.array - interaction matrix describing the relationship between the S
+    species (shape: (S+1, S+1), diagonal elements are set to 0)
+    '''
+
+    interaction_matrix = None
+
+    # generate the interaction matrix without the mutant
+    interaction_matrix = pre_interaction_matrix(
+                            S, 
+                            method = method, 
+                            seed = seed
+                            )
+
+    # perturb the species of interest (the last species)
+    interaction_row, interaction_col = mutant_interaction_matrix(
+                                        interaction_matrix[-1,:],
+                                        interaction_matrix[:,-1], 
+                                        seed = seed
+                                        )
+
+    # stack the perturbed interactions onto the interaction matrix
+    interaction_matrix = np.vstack([np.hstack([interaction_matrix, 
+                                            interaction_col[:,None]]), 
+                                    interaction_row[None,:]])
+
+    return interaction_matrix
---
+++ scripts/simulate_gLV.py
@@ -0,0 +1,328 @@
+
+__author__ = "Ammaar A. Saeed"
+__email__ = "aasaeed@college.harvard.edu"
+
+'''
+Description: 
+------------
+This file contains functions for simulating the generalized 
+Lotka-Volterra Model with evolution for a total of S species 
+under a stochastic framework, particularly a Moran process. 
+The functions expect parameters for the simulation (birth and 
+death rates, interaction matrix) to be already generated. 
+
+Implementation: 
+---------------
+The current model is based on a S+1-dimensional 
+continuous-time Moran process, with each dimension representing the
+population of a certain species. 
+We use a finite population Moran process model to simulate 
+the potential fixation of rng mutants that arise in the
+population for a single mutant, and we repeat these 
+simulations many times to estimate the fixation probabilities of
+the mutant. 
+As such, the simulation uses the embedded chain transition 
+matrix to determine the next state of the chain and then 
+calculates the exponential waiting time for this step 
+accordingly. Since there are technically two steps that 
+occur for each transition, we simply assign the time for 
+the longer step as the waiting time since the other will 
+have already happened by then. 
+'''
+
+import numpy as np 
+from numpy.random import default_rng
+
+# define a standard rng object for all functions 
+rng = default_rng()
+
+def calculate_moran_fixation_probability(
+        S: int, 
+        pop_size: int, 
+        birth_rates: np.array, 
+        death_rates: np.array
+    ):
+    '''
+    Calculate the fixation probability of the mutant species of 
+    interest, assuming a simple Moran process with no other 
+    interacting species
+    This model of the Moran process assumes that the birth and 
+    death rates are constant
+
+    Parameters: 
+    -----------
+    int S - total number of species 
+    int pop_size - population size for each present species 
+    np.array birth_rates - birth rates of each of the species 
+    np.array death_rates - death rates of each of the species
+
+    Returns: 
+    --------
+    float - baseline fixation probability of the mutant species
+    of interest
+    '''
+
+    # calculate the fitness of the species of interest and its 
+    # mutant
+    soi = birth_rates[-2] - death_rates[-2]
+    mutant_soi = birth_rates[-1] - death_rates[-1]
+
+    # calculate the relative fitness of the mutant species of 
+    # interest compared to the wild-type species of interest
+    relative_fitness = mutant_soi / soi
+
+    # calculate the fixation probability as discussed in lecture
+    return (1 - (1 / relative_fitness)) / (1 - (1 / relative_fitness) ** (S * pop_size))
+
+def calculate_adjusted_birth_rates(
+        system_state: np.array, 
+        birth_rates: np.array, 
+        interaction_matrix: np.array
+    ): 
+    '''
+    Calculate the adjusted birth rates for each of the species 
+    given the current state of the system 
+
+    Parameters:
+    -----------
+    np.array system_state - current populations of all species and 
+    mutant species of interest in the system 
+    np.array birth_rates - birth rates of each of the species 
+    np.array interaction_matrix - interaction matrix describing the 
+    the relationships between all species and the mutant species of
+    interest 
+
+    Returns: 
+    --------
+    np.array - array of adjusted birth rates for each of the species, 
+    adding only the positive interactions 
+    '''
+
+    return system_state * birth_rates + system_state @ \
+        ((interaction_matrix > 0) * interaction_matrix)
+
+
+def calculate_adjusted_death_rates(
+        system_state: np.array, 
+        death_rates: np.array, 
+        interaction_matrix: np.array
+    ): 
+    '''
+    Calculate the adjusted death rates for each of the species
+    given the current state of the system
+
+    Parameters:
+    -----------
+    np.array system_state - current populations of all species and 
+    mutant species of interest in the system 
+    np.array death_rates - death rates of each of the species 
+    np.array interaction_matrix - interaction matrix describing the 
+    the relationships between all species and the mutant species of
+    interest 
+
+    Returns: 
+    --------
+    np.array - array of adjusted death rates for each of the species, 
+    adding only the negative interactions 
+    '''
+
+    return system_state * death_rates + system_state @ \
+        ((interaction_matrix > 0) * interaction_matrix)
+
+def calculate_step_probability(
+        system_state: np.array, 
+        adj_birth_rates: np.array,
+        adj_death_rates: np.array
+    ): 
+    '''
+    Calculate the probabilities for each of the potential transitions
+    given the current state of the system. 
+    The possible transitions require that a single individual in 
+    one of the species births/replicates another individual of that
+    species, and a single individual in another of the species 
+    dies. We treat these two events as indepdent, scaling linearly
+    with the population sizes in accordance with minimum 
+    Exponential waiting times. 
+
+    Parameters: 
+    -----------
+    np.array system_state - current populations of all species and 
+    mutant species of interest in the system 
+    np.array adj_birth_rates - birth rates adjusted by contributions
+    from the interaction matrix 
+    np.array adj_death_rates - death rates adjusted by contributions 
+    from the interaction matrix
+
+    Returns:
+    --------
+    np.array - array of probabilities for potential transitions; 
+    probabilities are arranged in the order of the species given as 
+    first birth events for each species and then death events 
+    (shape: (2S,))
+    '''
+
+    # calculate the birth probabilities 
+    birth_probs = system_state * adj_birth_rates / \
+        np.sum(system_state * adj_birth_rates)
+
+    # calculate the death probabilities 
+    death_probs = system_state * adj_death_rates / \
+        np.sum(system_state * adj_death_rates)
+
+    return birth_probs, death_probs
+
+def run_step(
+        system_state: np.array, 
+        birth_rates: np.array, 
+        death_rates: np.array, 
+        interaction_matrix: np.array
+    ): 
+    '''
+    Run a single simulation step by taking in the current population 
+    of the system, calculating the transition probabilities, 
+    rngly determining the next transition, and drawing an 
+    exponential waiting time
+
+    Parameters: 
+    -----------
+    np.array system_state - current populations of all species and 
+    mutant species of interest in the system 
+    np.array birth_rates - birth rates of each of the species 
+    np.array death_rates - death rates of each of the species
+    np.array interaction_matrix - interaction matrix describing the 
+    the relationships between all species and the mutant species of
+    interest 
+
+    Returns: 
+    --------
+    np.array (1) - populations of all species and mutant species of 
+    interest in the system after the current step 
+    np.array (2) - waiting time for the process to reach this point 
+    '''
+
+    # calculate the adjusted birth and death rates 
+    adj_birth_rates = calculate_adjusted_birth_rates(
+        system_state, 
+        birth_rates, 
+        interaction_matrix
+    )
+    adj_death_rates = calculate_adjusted_death_rates(
+        system_state, 
+        death_rates, 
+        interaction_matrix
+    )
+
+    # calculate the transition probabilities according to these rates 
+    birth_probs, death_probs = calculate_step_probability(
+        system_state, 
+        adj_birth_rates, 
+        adj_death_rates
+    )
+
+    # determine the species replicating in this step 
+    unif_sample = rng.uniform(low = 0, high = 1)
+    birth_idx = np.nonzero(np.cumsum(birth_probs) > unif_sample)[0][0]
+
+    # renormalize the death probabilities 
+    remove_birth = np.ones(system_state.shape)
+    remove_birth[birth_idx] = 0
+    death_probs = death_probs * remove_birth
+    death_probs = death_probs / np.sum(death_probs)
+
+    # determine the species dying in this step 
+    unif_sample = rng.uniform(low = 0, high = 1)
+    death_idx = np.nonzero(np.cumsum(death_probs) > unif_sample)[0][0]
+
+    # simulate the exponential waiting time, taking the max of the minimum of
+    # exponential rng variables 
+
+    exp_wait_time = np.max(
+        np.array(
+            [
+                rng.exponential(scale = system_state[birth_idx] * \
+                    adj_birth_rates[birth_idx]), 
+                rng.exponential(scale = system_state[death_idx] * \
+                    adj_death_rates[death_idx])
+            ]
+        )
+    )
+    
+    # update the populations of the system 
+    system_state[birth_idx] += 1
+    system_state[death_idx] -= 1
+
+    return system_state, exp_wait_time
+
+def run_simulation(
+        S: int, 
+        pop_size: int, 
+        birth_rates: np.array, 
+        death_rates: np.array, 
+        interaction_matrix: np.array
+    ): 
+    '''
+    Run a simulation of the generalized Lotka-Volterra under the 
+    stochastic framework, with stopping conditions of either (i) 
+    the species of interest goes extinct or (ii) the mutant 
+    species of interest
+
+    Parameters:
+    -----------
+    int S - total number of species 
+    int pop_size - population size for each present species 
+    np.array birth_rates - birth rates of each of the species 
+    np.array death_rates - death rates of each of the species
+    np.array interaction_matrix - interaction matrix describing the 
+    the relationships between all species and the mutant species of
+    interest 
+
+    Returns: 
+    --------
+    bool - boolean data type that returns whether or not the mutant 
+    species of interest fixes (1 if the mutant fixes; 0 if the mutant
+    goes extinct)
+    np.array (1) - array of exponential wait times between transitions
+    (shape: (steps,))
+    np.array (2) - array of states describing the population sizes of 
+    each species at each step (shape: (steps, 3))
+    '''
+
+    # initialize the populations of each of the species 
+    system_state = np.hstack([np.repeat(pop_size, S-1), [pop_size - 1, 1]])
+
+    # create arrays to store the waiting times and states of the system
+    system_states = [list(system_state)]
+    exp_wait_times = list()
+
+    # set counter to count total number of steps/transitions made 
+    counter = 0
+
+    # use a while loop to determine if a stopping condition is met 
+    while (
+        system_state[-2] > 0 and 
+        system_state[-1] > 0 and 
+        counter < 10^6
+    ): 
+        
+        # simulate a single step in the loop 
+        system_state, exp_wait_time = run_step(
+                                        system_state, 
+                                        birth_rates, 
+                                        death_rates, 
+                                        interaction_matrix
+                                        )
+
+        # append the current information about the system trajectory
+        system_states.append(list(system_state))
+        exp_wait_times.append(exp_wait_time)
+
+
+    # determine the result of the simulation
+    if (system_state[-1] > 0): 
+        result = 1
+    else: 
+        result = 0 
+
+    # return the result of the simulation and the collected data
+    # for the entire simulation 
+    return result, np.array(system_states), np.array(exp_wait_times)
